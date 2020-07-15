e6e19f81a1471736822ac94588c5ad2ad6e2ded9
MacRae Linton <macrael@truss.works>
2020-07-15 11:42:42 -0700

    Make REDIS_HOST configurable and complete the documentation for exercise 2

--- README.md
+++ README.md
@@ -33,7 +33,19 @@ Try editing the webpage and get that serving in a new docker _container_
 Use the little website at /name_saver. It requires redis to be available. There are official redis images available here: https://hub.docker.com/_/redis
 
 1. Get a redis image running, (make sure you expose a port!) and try and get the dockling server to connect to it locally
-* if you get dockling running, visit http://localhost:8042/name_saver
+* get dockling running (`go run ./cmd/dockling`), visit http://localhost:8042/name_saver
 * it will tell you wether it can see redis or not. Once it does, it will use redis to save a list of names. 
 
 2. Get the dockling server to connect to it while it is _also_ running in a docker container
+* you will need to create your own bridge network to make the two containers be able to see eachother by hostname: https://docs.docker.com/network/bridge/ (the default bridge network does not route via hostname)
+* `docker network create dockling-net`
+* you can configure `dockling` with the environment variables REDIS_HOST and REDIS_PORT to try and connect to redis-in-docker
+* when they are both running, you should be able to visit http://localhost:8043/name_server and save names like before
+
+Here are two base64 encoded invocations that worked for me to run dockling and redis in containers and talk to eachother:
+
+ZG9ja2VyIHJ1biAtcCA4MDQzOjgwNDIgLWUgUkVESVNfSE9TVD1kb2NrbGluZy1yZWRpcyAtLW5ldCBkb2NrbGluZy1uZXQgZG9ja2xpbmc=
+
+ZG9ja2VyIHJ1biAtLW5hbWUgZG9ja2xpbmctcmVkaXMgLXAgNjM3OTo2Mzc5IC0tbmV0IGRvY2tsaW5nLW5ldCByZWRpcw==
+
+There's a couple ways to do this, you can either keep redis running and use `docker network disconnect/connect` to attach it to dockling-net or you can start a new container on dockling-net with `docker run`
--- cmd/dockling/main.go
+++ cmd/dockling/main.go
@@ -64,68 +64,70 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // namesHandler has a form for saving names and displays a list of names saved
-func namesHandler(w http.ResponseWriter, r *http.Request) {
+func namesHandler(redis_addr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// attempt to connect to redis
+		c, err := redis.Dial("tcp", redis_addr)
+		if err != nil {
+			if strings.HasSuffix(err.Error(), "connect: connection refused") ||
+				strings.HasSuffix(err.Error(), "no such host") {
+				fmt.Println("Redis Not Available on ", redis_addr)
 
-	// attempt to connect to redis
-	c, err := redis.Dial("tcp", ":6379")
-	if err != nil {
-		if strings.HasSuffix(err.Error(), "connect: connection refused") {
-			fmt.Println("Redis Not Available on port 6379")
-
-			// If redis is not connected, we just print a static page.
-			page := `
+				// If redis is not connected, we just print a static page.
+				page := `
 <html>
 <head>
 <title>Make Way For Docklings</title
 </head>
 <body>
 <h1>Redis Dockling!</h1>
-<p>redis is not available on port 6379</p>
+<p>redis is not available on %s</p>
 </body>`
 
-			fmt.Fprintf(w, page)
+				fmt.Fprintf(w, fmt.Sprintf(page, redis_addr))
+				return
 
-		} else {
-			// Error out.
-			fmt.Println("Unexpected Error Connecting to Redis: ", err)
-			http.Error(w, "Unexpected Error Connecting To Redis", 500)
+			} else {
+				// Error out.
+				fmt.Println("Unexpected Error Connecting to Redis: ", err)
+				http.Error(w, "Unexpected Error Connecting To Redis", 500)
+				return
+			}
+		}
+
+		// From here on, we're connected to redis
+
+		var body string
+		// get a list of things from redis.
+		names, err := redis.Strings(c.Do("LRANGE", "names", 0, -1))
+		if err != nil {
+			fmt.Println("ERR FROM REDIS", err)
+			http.Error(w, "Unexpected Error Reading From Redis", 500)
 			return
 		}
-	}
 
-	// From here on, we're connected to redis
-
-	var body string
-	// get a list of things from redis.
-	names, err := redis.Strings(c.Do("LRANGE", "names", 0, -1))
-	if err != nil {
-		fmt.Println("ERR FROM REDIS", err)
-		http.Error(w, "Unexpected Error Reading From Redis", 500)
-		return
-	}
-
-	// a simple form and a simple <ul>
-	bodyTemplate :=
-		`
+		// a simple form and a simple <ul>
+		bodyTemplate :=
+			`
 <form action="/name_saver/save_name">
     <label for="name">Save a name:</label>
     <input name="name" id="name" value="">
     <button>Save Name</button>
 </form>
-<p>you are connected to redis on port 6379<p>
+<p>you are connected to redis on %s<p>
 <p>Here is the list of names you've saved:<p>
 <ul>
 %s
 </ul>
 `
-	nameList := ""
-	for _, name := range names {
-		nameList = nameList + fmt.Sprintf("<li>%s</li>\n", name)
-	}
+		nameList := ""
+		for _, name := range names {
+			nameList = nameList + fmt.Sprintf("<li>%s</li>\n", name)
+		}
 
-	body = fmt.Sprintf(bodyTemplate, nameList)
+		body = fmt.Sprintf(bodyTemplate, redis_addr, nameList)
 
-	page := `
+		page := `
 <html>
 <head>
 <title>Make Way For Docklings</title
@@ -135,49 +137,53 @@ func namesHandler(w http.ResponseWriter, r *http.Request) {
 %s
 </body>`
 
-	renderedPage := fmt.Sprintf(page, body)
+		renderedPage := fmt.Sprintf(page, body)
 
-	fmt.Fprintf(w, renderedPage)
+		fmt.Fprintf(w, renderedPage)
+
+	}
 
 }
 
-func addNameHandler(w http.ResponseWriter, r *http.Request) {
-	// Get parameter from get params,
-	fmt.Println("Adding Name")
+func addNameHandler(redis_addr string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Get parameter from get params,
+		fmt.Println("Adding Name")
 
-	addedNames, ok := r.URL.Query()["name"]
+		addedNames, ok := r.URL.Query()["name"]
 
-	if !ok {
-		fmt.Println("NO KEYS")
-	}
+		if !ok {
+			fmt.Println("NO KEYS")
+		}
 
-	// attempt to connect to redis
-	c, err := redis.Dial("tcp", ":6379")
-	if err != nil {
-		// Error out.
-		fmt.Println("Error Connecting to Redis: ", err)
-		http.Error(w, "Error Connecting To Redis", 500)
-		return
-	}
-
-	// add a key to redis
-	for _, addedName := range addedNames {
-		fmt.Println("Adding: ", addedName)
-		_, err := c.Do("RPUSH", "names", addedName)
+		// attempt to connect to redis
+		c, err := redis.Dial("tcp", redis_addr)
 		if err != nil {
-			fmt.Println("BAD ADD ", err)
-			http.Error(w, "Error Adding To Redis", 500)
+			// Error out.
+			fmt.Println("Error Connecting to Redis: ", err)
+			http.Error(w, "Error Connecting To Redis", 500)
 			return
 		}
-	}
 
-	// redirect
-	http.Redirect(w, r, "/name_saver", 302)
+		// add a key to redis
+		for _, addedName := range addedNames {
+			fmt.Println("Adding: ", addedName)
+			_, err := c.Do("RPUSH", "names", addedName)
+			if err != nil {
+				fmt.Println("BAD ADD ", err)
+				http.Error(w, "Error Adding To Redis", 500)
+				return
+			}
+		}
+
+		// redirect
+		http.Redirect(w, r, "/name_saver", 302)
+	}
 }
 
-func serve(port string) {
-	http.HandleFunc("/name_saver", namesHandler)
-	http.HandleFunc("/name_saver/save_name", addNameHandler)
+func serve(port string, redis_addr string) {
+	http.HandleFunc("/name_saver", namesHandler(redis_addr))
+	http.HandleFunc("/name_saver/save_name", addNameHandler(redis_addr))
 	http.HandleFunc("/", helloHandler)
 	fmt.Printf("Serving HTTP on :%s\n", port)
 	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
@@ -192,6 +198,18 @@ func main() {
 		port = "8042"
 	}
 
-	serve(port)
+	redis_host := os.Getenv("REDIS_HOST")
+	if redis_host == "" {
+		redis_host = "localhost"
+	}
+
+	redis_port := os.Getenv("REDIS_PORT")
+	if redis_port == "" {
+		redis_port = "6379"
+	}
+
+	redis_addr := fmt.Sprintf("%s:%s", redis_host, redis_port)
+
+	serve(port, redis_addr)
 
 }
